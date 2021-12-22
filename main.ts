856667ee997d0d62ef49f221b5dc19cf59347a6b
npust7427 <96508740+npust7427@users.noreply.github.com>
2021-12-22 12:10:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,55 @@
+/**
+ * 轉彎角度控制
+ * 
+ * 800
+ * 
+ * 1200
+ * 
+ * 1500
+ */
+input.onButtonPressed(Button.A, function () {
+    Switch = 1
+})
+input.onButtonPressed(Button.B, function () {
+    Switch = 0
+})
 let sonar2 = 0
-let Switch = 1
+let Switch = 0
+Switch = 0
+/**
+ * 1. 超音波
+ * 
+ * 2. 行走、轉彎
+ * 
+ * 3. A鍵走、B鍵停
+ * 
+ * 4. 隨機轉彎
+ */
 basic.forever(function () {
-    if (sonar2 < 10) {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        0,
-        robotbit.Motors.M2A,
-        100
-        )
-    } else {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        100,
-        robotbit.Motors.M2A,
-        100
-        )
+    if (Switch == 1) {
+        if (sonar2 < 10) {
+            robotbit.MotorRunDual(
+            robotbit.Motors.M1A,
+            -100,
+            robotbit.Motors.M2A,
+            -100
+            )
+            basic.pause(1000)
+            robotbit.MotorRunDual(
+            robotbit.Motors.M1A,
+            -50,
+            robotbit.Motors.M2A,
+            100
+            )
+            basic.pause(800)
+        } else {
+            robotbit.MotorRunDual(
+            robotbit.Motors.M1A,
+            100,
+            robotbit.Motors.M2A,
+            100
+            )
+        }
     }
 })
 basic.forever(function () {
